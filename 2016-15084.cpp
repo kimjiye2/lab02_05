a7807372065ff2683737a5ae999ff7153f5049e3
jiye <lool1015@snu.ac.kr>
2018-04-09 12:47:37 +0900

    2016-15084

---
+++ 2016-15084.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include<functional>
+
+using namespace std;
+
+void main() {	
+		vector<int> v;
+		int num;
+		while (std::cin >> num) {
+			v.push_back(num);
+		}
+		
+		
+		sort(v.begin(), v.end(), greater<int>());
+		cout << " ordered array" << endl;
+
+		for (auto i : v) {
+			cout << i <<" ";
+		}
+}
